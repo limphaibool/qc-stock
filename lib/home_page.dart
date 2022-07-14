08f90cad87ef1fb2d378e9bd994f423c0744ce46
limphaibool <limphaibool@gmail.com>
2022-07-14 11:32:55 +0700

    ui input

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,184 +20,210 @@ class _HomePageState extends State<HomePage> {
   String? dateValue;
   String? timeValue;
   String? tableValue;
+  bool isChecked = false;
+  List checked = [];
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:
           AppBar(title: const Text('Mock UI', style: TextStyle(fontSize: 30))),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: DropdownButtonFormField(
-                    value: dateValue,
-                    items: <String>['1', '2', '3', '4']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'วันที่',
-                      labelStyle: TextStyle(fontSize: 25),
-                    ),
-                    onChanged: (String? value) {
-                      setState(() {
-                        dateValue = value;
-                      });
-                    },
-                  ),
-                ),
-                SizedBox(width: 20),
-                Expanded(
-                  flex: 2,
-                  child: DropdownButtonFormField(
-                    value: timeValue,
-                    items: <String>['13:00', '14:00', '15:00', '16:00']
-                        .map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'เวลาทํางาน',
-                      labelStyle: TextStyle(fontSize: 25),
-                    ),
-                    onChanged: dateValue == null
-                        ? null
-                        : (String? value) {
-                            setState(() {
-                              timeValue = value;
-                            });
-                          },
-                    disabledHint: Text(
-                      'โปรดเลือกวันที่',
-                      style: TextStyle(color: Colors.grey),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: DropdownButtonFormField(
+                      value: dateValue,
+                      items: <String>['1', '2', '3', '4']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'วันที่',
+                        labelStyle: TextStyle(fontSize: 25),
+                      ),
+                      onChanged: (String? value) {
+                        setState(() {
+                          dateValue = value;
+                        });
+                      },
                     ),
                   ),
-                ),
-                SizedBox(width: 20),
-                Expanded(
-                  flex: 2,
-                  child: DropdownButtonFormField(
-                    value: tableValue,
-                    items: <String>[
-                      'โต๊ะ 1',
-                      'โต๊ะ 2',
-                      'โต๊ะ 3',
-                      'โต๊ะ 4',
-                      'โต๊ะ 5'
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'โต๊ะ',
-                      labelStyle: TextStyle(fontSize: 25),
-                    ),
-                    onChanged: timeValue == null
-                        ? null
-                        : (String? value) {
-                            setState(() {
-                              tableValue = value;
-                            });
-                          },
-                    disabledHint: Text(
-                      'โปรดเลือกเวลาทํางาน',
-                      style: TextStyle(color: Colors.grey),
+                  SizedBox(width: 20),
+                  Expanded(
+                    flex: 2,
+                    child: DropdownButtonFormField(
+                      value: timeValue,
+                      items: <String>['13:00', '14:00', '15:00', '16:00']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'เวลาทํางาน',
+                        labelStyle: TextStyle(fontSize: 25),
+                      ),
+                      onChanged: dateValue == null
+                          ? null
+                          : (String? value) {
+                              setState(() {
+                                timeValue = value;
+                              });
+                            },
+                      disabledHint: Text(
+                        'โปรดเลือกวันที่',
+                        style: TextStyle(color: Colors.grey),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(width: 30),
-                Expanded(
-                  flex: 1,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      primary: Colors.white,
-                      padding: EdgeInsets.symmetric(vertical: 10),
+                  SizedBox(width: 20),
+                  Expanded(
+                    flex: 2,
+                    child: DropdownButtonFormField(
+                      value: tableValue,
+                      items: <String>[
+                        'โต๊ะ 1',
+                        'โต๊ะ 2',
+                        'โต๊ะ 3',
+                        'โต๊ะ 4',
+                        'โต๊ะ 5'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'โต๊ะ',
+                        labelStyle: TextStyle(fontSize: 25),
+                      ),
+                      onChanged: timeValue == null
+                          ? null
+                          : (String? value) {
+                              setState(() {
+                                tableValue = value;
+                              });
+                            },
+                      disabledHint: Text(
+                        'โปรดเลือกเวลาทํางาน',
+                        style: TextStyle(color: Colors.grey),
+                      ),
                     ),
-                    child: Text(
-                      'ค้นหา',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    onPressed: () {},
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Table(
-              border: TableBorder.all(width: 1, color: Colors.grey),
-              children: [
-                TableRow(
-                  children: [
-                    tableHeader('header'),
-                    tableHeader('header'),
-                    tableHeader('header'),
-                    tableHeader('header'),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                    tableItem('data'),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 40),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-                primary: Colors.white,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  SizedBox(width: 30),
+                  Expanded(
+                    flex: 1,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        primary: Colors.white,
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                      ),
+                      child: Text(
+                        'ค้นหา',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
               ),
-              child: Text(
-                'ยืนยัน',
-                style: TextStyle(fontSize: 20),
+              SizedBox(height: 30),
+              Table(
+                border: TableBorder.all(width: 1, color: Colors.grey),
+                children: [
+                  TableRow(
+                    children: [
+                      tableHeader('header'),
+                      tableHeader('header'),
+                      tableHeader('header'),
+                      tableHeader('header'),
+                      tableHeader('header'),
+                      tableHeader('header'),
+                      tableHeader('header'),
+                      tableHeader('header'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      tableCheckBox(),
+                      tableInput(),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                    ],
+                  ),
+                  TableRow(
+                    children: [
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                      tableItem('data'),
+                    ],
+                  ),
+                ],
               ),
-              onPressed: () {},
-            )
-          ],
+              SizedBox(height: 40),
+              TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  primary: Colors.white,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                ),
+                child: Text(
+                  'ยืนยัน',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  print(checked);
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -212,6 +238,37 @@ class _HomePageState extends State<HomePage> {
         text,
         style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
       ),
+    )));
+  }
+
+  Widget tableInput() {
+    return TableCell(
+        child: Center(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  onChanged: (value) {
+                    input = value;
+                  },
+                ))));
+  }
+
+  Widget tableCheckBox() {
+    return TableCell(
+        child: Center(
+            child: Checkbox(
+      value: isChecked,
+      onChanged: (bool? value) {
+        setState(() {
+          isChecked = value!;
+          if (isChecked) {
+            checked.add('1');
+          } else {
+            checked.remove('1');
+          }
+        });
+      },
     )));
   }
 
